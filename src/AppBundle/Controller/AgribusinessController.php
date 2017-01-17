0a6cee26c906f7cb56e0515f1df8b10bbdcc13b1
samzerkel <samize97@gmail.com>
2017-01-17 13:02:44 +0100

    controller and twig for agribusiness page created 1

---
+++ src/AppBundle/Controller/AgribusinessController.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Description of AgribusinessController
+ *
+ * @author samiz
+ */
+class AgribusinessController extends Controller {
+    //put your code here
+    /**
+     * @Route("agribusiness")
+     */
+    public function agribusinessConsultnacy () {
+        return $this->render('consultancy/agribusiness.html.twig');
+        
+    }
+}
