4ddddb3e452d9e22f53ddcf8898cfc9c8830fbb4
eyobiel <eyobielyebio@gmail.com>
2017-01-18 17:05:06 +0100

    navbars removed from index.html and added in the base.html and ConsultancyController method and blocks modified

--- src/AppBundle/Controller/ConsultancyController.php
+++ src/AppBundle/Controller/ConsultancyController.php
@@ -1,40 +1,32 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace AppBundle\Controller;
+/**
+ * This ConsultancyController is created for the home page of the consultancy agency website. 
+ *
+ * @author Eyobiel
+ */
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-/**
- * Description of ConsultancyController
- *
- * @author gebruiker
- */
+
 class ConsultancyController extends Controller {
-    //put your code here
+
     /**
+     * This method renders a response index.html.twig (homepage) from the consultancy folder inside views
+     *
      * @Route("/consultancy")
+     *
+     * @param $request Request
+     *
+     * @return Response
      */
-    
-    public function consultAction() {
-        $navigation = [
-            'agricultural'=>'Agricultural Science',
-            'environmental'=>'Environmental Science',
-            'agribusiness'=>'Agribusiness Management',
-            'contactform'=> 'Contact'
-        ];
-      
-        return $this->render('consultancy/index.html.twig', array(
-            
-            
-            'services' => $navigation
-        ));
-        
+    public function consultAction(Request $request) {
+        $response = $this->render('consultancy/index.html.twig');
+
+        return $response;
     }
 }
